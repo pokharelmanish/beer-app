2453343f9a8df70934353ebfb80c4816161ffca1
pokharelmanish <pokharelman@gmail.com>
2020-08-14 10:50:10 -0700

    Updates

---
+++ precache-manifest.69a0570a4542d779fd2d0ff4f86fff11.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0121c83ec67c30a7e1be4b4c57863f12",
+    "url": "/beer-app/index.html"
+  },
+  {
+    "revision": "34c9159f60d8202d84ba",
+    "url": "/beer-app/static/js/2.f91d66cf.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/beer-app/static/js/2.f91d66cf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9f7a5205770383bce258",
+    "url": "/beer-app/static/js/main.558a2ce4.chunk.js"
+  },
+  {
+    "revision": "86e2902030d4ae09d51b",
+    "url": "/beer-app/static/js/runtime-main.4abdf314.js"
+  }
+]);
